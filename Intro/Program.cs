f9b6b6aba5aa39c40d1cae5874bc692bb77e65f6
zano <adrian@xenotype.com>
2018-04-06 14:11:16 +0200

    Add project files.

---
+++ Intro/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Intro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hej NG18!");
+
+            string name = Ui.Ask("Vad heter du?");
+            Console.WriteLine($"Nämen hej \"{name}\"!");
+            
+            int age = Ui.AskForInt("Hur gammal är du? ");
+            if (age < 20)
+            {
+                Console.WriteLine("Du får inte ännu handla på Systemet.");
+            }
+            else
+            {
+                Console.WriteLine("Plockar du upp en flaska Gnarly Head på vägen hem?");
+            }
+        }
+
+       
+    }
+}
---
+++ Intro/Ui.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Intro
+{
+    class Ui
+    {
+        public static int AskForInt(string question)
+        {
+            bool parsed;
+            int value;
+            do
+            {
+                string answer = Ask(question);
+                parsed = int.TryParse(answer, out value);
+                if (!parsed)
+                {
+                    Console.WriteLine("Du får bara använda siffror i svaret");
+                }
+
+            } while (!parsed);
+            return value;
+        }
+
+        public static string Ask(string question)
+        {
+            Console.Write(question + " ");
+            return Console.ReadLine();
+        }
+    }
+}
